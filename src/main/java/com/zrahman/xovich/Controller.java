ae93d401aa2424e647e60876aa11ff29952461ac
Zishan Rahman <zishanfrahman@outlook.com>
2022-08-03 23:36:57 +0100

    Organise imports (remove unused import) in Controller class

--- src/main/java/com/zrahman/xovich/Controller.java
+++ src/main/java/com/zrahman/xovich/Controller.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 
 public class Controller {
     @FXML
